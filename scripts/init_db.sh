1de7057d47c85c34e1a055b2df50cd478305e92c
Yago Iglesias <yagoiglesias7@gmail.com>
2023-08-27 12:09:06 +0200

    fix: change port to 5069

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -23,7 +23,7 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5432}"
+DB_PORT="${POSTGRES_PORT:=5069}"
 # Launch postgres using Docker
 docker run  -e POSTGRES_USER=${DB_USER}  -e POSTGRES_PASSWORD=${DB_PASSWORD}  -e POSTGRES_DB=${DB_NAME} -p "${DB_PORT}":5432  -d postgres  postgres -N 1000
 # ^ Increased maximum number of connections for testing purposes
