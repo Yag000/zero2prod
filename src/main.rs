127f62520956351b5dc034c975f09654f786aa14
Yago Iglesias <yagoiglesias7@gmail.com>
2023-09-01 20:25:47 +0200

    feat(docker): Basic docker config, needs some extra work

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,27 @@ use zero2prod::{configurations::get_configuration, startup::run, telemetry::get_
 async fn main() -> Result<(), std::io::Error> {
     // Logger setup
     LogTracer::init().expect("Failed to set logger");
+
     // Info or above will be logged if the RUST_LOG environment variable is not set
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     set_global_default(subscriber).expect("Failed to set subscriber");
 
     // We want to panic if we cannot read the configuration
     let configuration = get_configuration().expect("Failed to read configurations");
-    let connection = PgPool::connect(
+    let connection = PgPool::connect_lazy(
         configuration
             .database
             .get_connnection_string()
             .expose_secret(),
     )
-    .await
-    .expect("Failed to connect to Postgresf");
+    .expect("Failed to connect to Postgres");
+
     // Bind the TCP listener socket address with the configuration port
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener, connection)?.await
 }
